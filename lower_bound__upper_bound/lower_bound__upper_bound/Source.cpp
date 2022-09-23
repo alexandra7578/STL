27f3e4d0bafac7e6f4c36be3f3425235fc06729d
Alexandra <enescualexandra03@yahoo.com>
2022-09-23 10:58:46 +0300

    add lower-upper bound

---
+++ lower_bound__upper_bound/lower_bound__upper_bound/Source.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int main()
+{
+	int arr[] = { 5, 10, 15, 20, 20, 23, 42, 45 };
+	int  n = sizeof(arr) / sizeof(arr[0]);
+	vector<int>vect(arr, arr + n);
+	sort(vect.begin(), vect.end());
+	auto q = upper_bound(vect.begin(), vect.end(), 20);
+	auto p = lower_bound(vect.begin(), vect.end(), 20);
+	cout << "The lower bound is at position: ";
+	cout << p - vect.begin() << endl;
+	
+	cout << "The upper bound is at position: ";
+	cout << q - vect.begin() << endl;
+}
