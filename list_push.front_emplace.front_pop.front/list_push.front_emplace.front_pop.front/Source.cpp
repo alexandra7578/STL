ed828b53fdede9bcd30a58244db48da4ec40b875
Alexandra <enescualexandra03@yahoo.com>
2022-10-03 13:26:30 +0300

    add list pop_front emplace_front pop_front

---
+++ list_push.front_emplace.front_pop.front/list_push.front_emplace.front_pop.front/Source.cpp
@@ -0,0 +1,23 @@
+#include <forward_list>
+#include <iostream>
+using namespace std;
+int main()
+{
+	forward_list<int> flist = { 10,20,30,40,50 };
+	flist.push_front(60);
+	cout << "The forward list after push_front operation: ";
+	for (int& c : flist)
+		cout << c << " ";
+	cout << endl;
+	flist.emplace_front(70);
+	cout << "The forward list after emplace_front operation: ";
+	for (int& c : flist)
+		cout << c << " ";
+	cout << endl;
+	flist.pop_front();
+	cout << "The forward list after pop_front operation: ";
+	for (int& c : flist)
+		cout << c << " ";
+	cout << endl;
+	return 0;
+}
