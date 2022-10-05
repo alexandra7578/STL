3304cb1b9b665ed38a11b79d6ba6a3c591a30743
Alexandra <enescualexandra03@yahoo.com>
2022-10-05 09:48:47 +0300

    add set

---
+++ set/set/Source.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <set>
+int main()
+{
+	std::set<char> a;
+	a.insert('G');
+	a.insert('F');
+	a.insert('G');
+	for (auto& str : a)
+		std::cout << str << " ";
+	std::cout <<"\n";
+	return 0;
+}
