67ad0d05390e6ff8b4205bb74edebcbae9716b45
Alexandra <enescualexandra03@yahoo.com>
2022-10-07 13:09:29 +0300

    add unordered map

---
+++ unordered_map_functionality/unordered_map_functionality/Source.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+int main()
+{
+	unordered_map<string, double> umap;
+	umap["PI"] = 3.14;
+	umap["root2"] = 1.414;
+	umap["root3"] = 1.732;
+	umap["log10"] = 2.302;
+	umap["loge"] = 1.0;
+	umap.insert(make_pair("e", 2.718));
+	string key = "PI";
+	if (umap.find(key) == umap.end())
+		cout << key << " not found\n\n";
+	else
+		cout << "Found" << key << endl;
+	key = "lambda";
+	if (umap.find(key) == umap.end())
+		cout << key << "not found\n";
+	else
+		cout << "Found" << key << endl;
+	unordered_map<string, double>::iterator itr;
+	cout << "\n All elem: \n";
+	for (itr = umap.begin(); itr != umap.end(); itr++)
+		cout << itr->first << " " << itr->second << endl;
+}
